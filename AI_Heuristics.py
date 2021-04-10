be0df0c4b7f88fd207816b1ff2329a0636efefd9
Wilson Wu <wilson.wu.002@gmail.com>
2021-04-10 15:51:30 -0700

    rewrote the heuristics for monotonicity

--- AI_Heuristics.py
+++ AI_Heuristics.py
@@ -1,27 +1,45 @@
 import numpy as np, math
 from copy import deepcopy
-weight_table = [[1.5, 1, 0.5, 0],[2, 2.5, 3, 3.5],[7, 6, 5, 4],[8, 10, 12, 14]]
 def heuristics(grid, num_empty):
   '''
   This function scores the grid based on the algorithm implemented
   so that the maximize function of AI_Minimax can decide which branch
   to follow.
-  '''
-  grid = np.array(grid)
-  score = 0
-  max_val = 0
-  max_r, max_c = 0, 0
-  # TODO: Implement your heuristics here. 
+  '''# TODO: Implement your heuristics here.
   # You are more than welcome to implement multiple heuristics
   
-  #checking for monotonicity
+  grid = np.array(grid)
+    
+  #checking that the largest tile is bottom right
+  max_val = 0
+  max_r, max_c = 0, 0
+  large_tile_penalty = 0
   for i in range(len(grid)):
     for j in range(len(grid[i])):
-        score += grid[i][j] * 2**weight_table[i][j]
         if grid[i][j] >= max_val:
             max_val = grid[i][j]
             max_r = i
             max_c = j
+  large_tile_penalty = 6 - max_r - max_c
   
-  # Weight for each score
-  return score * num_empty / (max_val ** (6 - max_r - max_c))
+  # checking monotonicity
+  # check that it's increasing from left to right
+  score = 0
+  for i in range(len(grid)):
+    for j in range(len(grid[i]) - 1):
+      if(grid[i][j] > grid[i][j + 1]):
+        score -= 1
+      else:
+        score += 1
+
+  # check that it's increasing from top to bottom
+  for i in range(len(grid)):
+    for j in range(len(grid[i]) - 1):
+      if(grid[j][i] > grid[j + 1][i]):
+        score -= 1
+      else:
+        score += 1
+        
+  # check smoothness
+  smoothness = 0
+  return score - large_tile_penalty + smoothness + num_empty
